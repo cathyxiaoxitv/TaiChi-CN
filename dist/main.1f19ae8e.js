43a145d3b23dcedbfaa783b51f5abdaa49eaca69
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-07-02 16:49:08 +0900

    again

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -185,7 +185,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55798" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56580" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -362,4 +362,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
